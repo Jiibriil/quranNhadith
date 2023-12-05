673baa90c7743489b487132ffcf46eae16ae12e1
Jibril Mohamud <jibrilmohamod@gmail.com>
2023-12-05 16:57:50 +0300

    Add dependencies for Google Fonts, Nuxt API Party, Vueuse, and Kinde

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-api-party",
+    "@nuxtjs/google-fonts",
+    "@vueuse/nuxt",
+    "@nuxtjs/kinde"
+  ]
 })
